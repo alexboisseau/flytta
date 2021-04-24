d65b91f8462a61d5c5c6656250bd2a963bd38b9d
Thomas Le Naour <lenaourthomas@gmail.com>
2021-04-24 17:09:30 +0200

    add chat view

--- Application/src/screens/app/chat/conversation.screen.js
+++ Application/src/screens/app/chat/conversation.screen.js
@@ -1,4 +1,41 @@
-import React from 'react';
-import { Text } from '../../../components/ui/text';
+import React, { useContext, useEffect, useState } from 'react';
+import { FlatList, ScrollView } from 'react-native-gesture-handler';
 
-export const ChatConversationScreen = () => <Text>MessagesScreen</Text>;
+import { AuthenticationContext } from '../../../services/authentication/authentication.context';
+
+import { Bubble } from './components/conversation-card.styles';
+import { Text } from '../../../components/ui/text';
+import { SafeArea } from '../../../components/utility/safe-area';
+import { getMessages } from '../../../services/conversations/conversations.service';
+
+export const ChatConversationScreen = ({ route }) => {
+  const { conversation } = route.params;
+  const { user } = useContext(AuthenticationContext);
+  const [messages, setMessages] = useState([]);
+
+  const fetchMessages = async () => {
+    try {
+      const allMessages = await getMessages(conversation.id);
+      setMessages(allMessages);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchMessages();
+  }, []);
+
+  return (
+    <SafeArea>
+      {messages.map((message) => {
+        return (
+          <Bubble orange={user.uid === message.sender.userId ? true : false}>
+            <Text bold>{message.sender.firstName}</Text>
+            <Text>{message.content}</Text>
+          </Bubble>
+        );
+      })}
+    </SafeArea>
+  );
+};
--- Application/src/screens/app/chat/home.screen.js
+++ Application/src/screens/app/chat/home.screen.js
@@ -1,4 +1,45 @@
-import React from 'react';
-import { Text } from '../../../components/ui/text';
+import React, { useContext, useEffect, useState } from 'react';
+import { TouchableOpacity } from 'react-native';
 
-export const ChatHomeScreen = () => <Text>MessagesScreen</Text>;
+import { AuthenticationContext } from '../../../services/authentication/authentication.context';
+
+import { SafeArea } from '../../../components/utility/safe-area';
+import { getConversations } from '../../../services/conversations/conversations.service';
+import { ConversationCard } from './components/conversation-card.component';
+import { FlatList } from 'react-native-gesture-handler';
+
+export const ChatHomeScreen = ({ navigation }) => {
+  const { user } = useContext(AuthenticationContext);
+  const [conversations, setConversations] = useState([]);
+
+  const fetchConversations = async () => {
+    try {
+      const allConversations = await getConversations(user.uid);
+      setConversations(allConversations);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchConversations();
+  }, []);
+
+  return (
+    <SafeArea>
+      <FlatList
+        data={conversations}
+        renderItem={({ item }) => (
+          <TouchableOpacity
+            onPress={() =>
+              navigation.navigate('ChatConversation', { conversation: item })
+            }
+          >
+            <ConversationCard conversation={item} />
+          </TouchableOpacity>
+        )}
+        keyExtractor={(conversation) => conversation.id}
+      />
+    </SafeArea>
+  );
+};
